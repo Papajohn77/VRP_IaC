a52947c98a287a0c0a9c0c03c91cdca6486aa5b9
Papajohn77 <72808287+Papajohn77@users.noreply.github.com>
2023-07-21 17:53:19 +0300

    Initial commit

---
+++ vrp_env/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.28.0"
+    }
+  }
+}
+
+variable "domain_name" {}
+
+provider "digitalocean" {}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = "vrp-env"
+}
+
+resource "digitalocean_droplet" "vrp-env" {
+  image      = "ubuntu-22-10-x64"
+  name       = "vrp-env"
+  region     = "fra1"
+  size       = "s-1vcpu-1gb-intel"
+  monitoring = true
+  ssh_keys   = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+resource "digitalocean_domain" "domain" {
+  name = var.domain_name
+  ip_address = digitalocean_droplet.vrp-env.ipv4_address
+}
+
+resource "digitalocean_record" "cname-record-vrp-solver" {
+  domain = var.domain_name
+  type   = "CNAME"
+  name   = "vrp-solver"
+  value  = format("%s.", var.domain_name)
+  depends_on = [digitalocean_domain.domain]
+}
+
+resource "digitalocean_record" "cname-record-vrp-solver-api" {
+  domain = var.domain_name
+  type   = "CNAME"
+  name   = "vrp-solver-api"
+  value  = format("%s.", var.domain_name)
+  depends_on = [digitalocean_domain.domain]
+}
+
+resource "digitalocean_record" "cname-record-phpmyadmin" {
+  domain = var.domain_name
+  type   = "CNAME"
+  name   = "phpmyadmin"
+  value  = format("%s.", var.domain_name)
+  depends_on = [digitalocean_domain.domain]
+}
+
+resource "digitalocean_project" "vrp-solver-project" {
+  name        = "VRP_Solver"
+  purpose     = "Web Application"
+  environment = "Production"
+  resources   = [
+    digitalocean_droplet.vrp-env.urn,
+    digitalocean_domain.domain.urn
+  ]
+}
+
+resource "local_file" "vrp-env-ipv4" {
+  filename = "./vrp-env-ipv4"
+  content = digitalocean_droplet.vrp-env.ipv4_address
+}
